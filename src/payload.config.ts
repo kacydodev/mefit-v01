131bdff134dc08e4b3590e576777de064ca05070
kacydodev <contact@kacydo.dev>
2026-08-10 17:20:26 +0700

    fix error uploading to railway s3 storage "Region is missing"

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -76,7 +76,7 @@ export default buildConfig({
           accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
           secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
         },
-        region: process.env.AWS_DEFAULT_REGION || '',
+        region: process.env.AWS_DEFAULT_REGION || 'auto',
         forcePathStyle: true,
       },
     }),
